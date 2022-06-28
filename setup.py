dfee469edf2c9c2cc7d50a983f64d94029cc181d
Sebastian Barry <sebastianbarry02@gmail.com>
2022-06-28 17:05:49 -0400

    pip files

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+with open("README.md", 'r', encoding="utf-8") as f: 
+    long_description = f.read()
+
+setuptools.setup(
+        name='toy_dl', 
+        version='0.0.1', 
+        author='Sebastian Barry', 
+        author_email='sebastianbarry02@gmail.com', 
+        long_description=long_description, 
+        description='Toy Deep Learning Framework',
+        url='https://github.com/sebbarry/toy-deeplearning-framework.git', 
+        license='MIT', 
+        install_requires=['numpy'],
+        packages=['toy_dl'],
+        )
+
+
